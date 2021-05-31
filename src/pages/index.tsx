4b44315d033523cafc89ba4019126d69d0269589
Diego Ricardo <diegoricardoweb@gmail.com>
2021-05-30 22:24:18 -0300

    creating dynamic routes to place pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
+import HomeTemplate from 'templates/Home'
 import { MapProps } from 'components/Map'
 import client from 'graphql/client'
-import { GetPlacesQuery } from 'graphql/generated/graphql'
 import { GET_PLACES } from 'graphql/queries'
-
-import HomeTemplate from 'templates/Home'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
 
 export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />
